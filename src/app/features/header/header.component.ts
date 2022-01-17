2984a0a9e2f3bbfd4bd15a09d0085c55fb34f3a0
BLADINE <59175711+BLADINE@users.noreply.github.com>
2022-01-17 23:23:27 +0100

    achieve css add header
    all things work well

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  background: Record<string, string> = {};
+  constructor(public router:Router) { }
 
   ngOnInit(): void {
   }
-
+  backgroundLink(){
+    if(this.router.url=="/home"){
+       this.background={
+                        "background-color":"aqua",
+                        "color":"black"
+                      }
+    }
+    else if (this.router.url=="/details"){
+       this.background={
+                        "background-color":"aqua"
+                        ,"color":"black"
+                      }
+    }
+    
+  }
 }
