46455417fee39a60bee45eeec6b34122305002bc
BLADINE <59175711+BLADINE@users.noreply.github.com>
2022-01-17 10:46:35 +0100

    Add css on details-produit
    I made some modifications on the view dtails-produit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../core/services/products.service';
-
+import { Product } from '../../core/Interfaces/product';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  onDisplayList=false;
+  onDisplayList!:boolean;
+  productsList!:Product[];
   constructor(private productsService:ProductsService) { }
 
   ngOnInit(): void {
   }
   onGetDetailsProduct(displayList:boolean){
+    this.onDisplayList=false
     console.log("Received an event from the component btn-details", displayList)
-    // this.productsService.getProductsFromJson()
-    //       .subscribe
+    this.productsService.getProductsFromJson()
+          .subscribe({
+            next:(res:Product[])=>{
+              this.productsList=[...res];
+              console.log(this.productsList);},
+            // error:(err)=>console.log("I got an error "+err),
+            // complete:()=>{this.onDisplayList=!this.onDisplayList}
+          })
+    //if(this.onDisplayList)
+      
   }
   
 
